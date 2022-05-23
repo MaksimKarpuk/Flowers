f7d290e1eea9c8759aedc24ce7846c52a7404c61
Maksim <maksim.karpuk13910@gmail.com>
2022-05-23 19:33:19 +0300

    flowers/FT-1

---
+++ Flowers/nuxt.config.js
@@ -0,0 +1,63 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  target: 'static',
+
+  head: {
+    title: 'Flowers',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    {
+      src:"~/assets/styles/index.scss",
+      lang:"sass"
+    }
+  ],
+  styleResources: {
+    scss: [
+      "~/assets/styles/colors.scss"
+    ],
+  },
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    "~/plugins/vue-lazyload.js"
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+    // https://go.nuxtjs.dev/stylelint
+    '@nuxtjs/stylelint-module',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    '@nuxtjs/style-resources',
+    '@nuxtjs/svg',
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    baseURL: '/',
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+}
---
+++ Flowers/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <img v-lazy="'/img/1.jpeg'" alt=""/>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
